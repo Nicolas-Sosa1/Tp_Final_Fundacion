65ffd0dbec2e3c81b7927d3169dd0a22f6fce6ff
Ever Diaz <ever_2830@hotmail.com>
2025-11-23 13:48:42 -0300

    Delete Tp_Final_Fundacion-main/server directory

--- Tp_Final_Fundacion-main/server/routes/users.routes.js
+++
@@ -1,9 +0,0 @@
-import {Router} from "express"
-import userController from "../controllers/users.controller.js"
-
-const usersRoutes = Router();
-
-usersRoutes.post('/register', userController.createOne);
-usersRoutes.post('/login', userController.login);
-
-export default usersRoutes
